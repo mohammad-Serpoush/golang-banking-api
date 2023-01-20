257a2d8063a263036025c458ff77a9357ecfa9bd
mohammad serpoush <mohammad.serpush@gmail.com>
2023-01-20 16:34:01 +0330

    add login with paseto token api

--- api/server.go
+++ api/server.go
@@ -1,25 +1,45 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/go-playground/validator/v10"
 	db "github.com/mohammad-Serpoush/golang-banking-api/db/sqlc"
+	"github.com/mohammad-Serpoush/golang-banking-api/token"
+	"github.com/mohammad-Serpoush/golang-banking-api/util"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
+	config     util.Config
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -28,9 +48,8 @@ func NewServer(store db.Store) *Server {
 
 	router.POST("/transfers", server.createTransfer)
 	router.POST("/users", server.createUser)
-
+	router.POST("/users/login", server.loginUser)
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
